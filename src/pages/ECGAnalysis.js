a68076c41204d6ccb2f86a2434bef7478089625e
alenzer <alenzer0902@gmail.com>
2022-10-18 06:16:06 -0400

    endpoint

--- src/pages/ECGAnalysis.js
+++ src/pages/ECGAnalysis.js
@@ -1,16 +1,26 @@
 import React from "react";
 
 const a = () => {
-    return (
-        <div style={{ position: "fixed", top: 100, bottom: 0, left: 0, right: 0, backgroundColor: 'white' }}>
-            <iframe
-                allowFullScreen
-                width={"100%"}
-                height={"100%"}
-                frameBorder="0"
-                src={"https://ecg.cardiolyse.com/sign-in"}
-            />
-        </div>
-    )
-}
-export default a
+  return (
+    <div
+      style={{
+        position: "fixed",
+        top: 100,
+        bottom: 0,
+        left: 0,
+        right: 0,
+        backgroundColor: "white",
+      }}
+    >
+      <iframe
+        // title="ecg live"
+        allowFullScreen
+        width={"100%"}
+        height={"100%"}
+        frameBorder="0"
+        src={"https://app.cardiolyse.com/patients"}
+      />
+    </div>
+  );
+};
+export default a;
